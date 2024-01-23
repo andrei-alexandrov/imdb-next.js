f64306c3ee00f7ee4419b6e98fa5ef5664db0d02
Andrei Alexandrov <andrei.alxv@gmail.com>
2024-01-23 18:17:05 +0200

    added header section

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function MenuItem({ title, address, Icon }) {
+  return (
+    <Link className="hover:text-amber-600" href={address}>
+      <Icon className="text-2xl sm:hidden " />
+      <p className="uppercase hidden sm:inline text-sm">{title}</p>
+    </Link>
+  );
+}
