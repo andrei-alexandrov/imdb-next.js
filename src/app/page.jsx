c94dbf8b51c66051b5ee1c28b5698c9e0d00bfbe
Andrei Alexandrov <andrei.alxv@gmail.com>
2024-01-24 19:08:37 +0200

    added fetch functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div>HomePage</div>;
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data or API is down, try again later");
+  }
+
+  const results = data.results;
+  console.log(data.results);
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
