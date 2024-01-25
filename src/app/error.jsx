6972c6be991395a598a96ecd71727a3e305eecde
Andrei Alexandrov <andrei.alxv@gmail.com>
2024-01-25 14:13:03 +0200

    added details page component

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -10,7 +10,14 @@ export default function error({ error, reset }) {
   return (
     <div className="text-center mt-10">
       <p>Something went wrong, please try again later.</p>
-      <button className="hover: text-amber-600" onClick={() => reset()}>Try again</button>
+      <button
+        className="bg-amber-100 text-gray-800  font-semibold 
+        py-2 px-4 mt-6 dark:bg-gray-600 dark:text-white hover:text-amber-600
+        dark:hover:text-amber-600"
+        onClick={() => reset()}
+      >
+        Try again
+      </button>
     </div>
   );
 }
